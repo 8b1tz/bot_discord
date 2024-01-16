d81613f0bce495f502eda5f98f866ff0a7e4dfea
FerndWehbe <51804490+FerndWehbe@users.noreply.github.com>
2024-01-15 23:54:09 -0300

    Change in the way badges are returned

--- app/scripts/get_db_or_api.py
+++ app/scripts/get_db_or_api.py
@@ -1,7 +1,12 @@
 from habblet_api import HabbletApi
-from models import Enable, Handitem
-from repository import (get_enable_by_id, get_handitem_by_id, insert_enable,
-                        insert_handitem)
+from models import Enable, Handitem, Badge
+from repository import (
+    get_enable_by_id,
+    get_handitem_by_id,
+    get_wired_by_id,
+    insert_enable,
+    insert_handitem,
+)
 
 
 def get_enable_by_id_or_api(enable_id: str):
@@ -24,7 +29,9 @@ def get_hand_item_by_id_or_api(hand_item_id: str):
         return hand_item
     hb = HabbletApi()
     list_hand_item = hb.get_handitem()
-    filtered_enable = list(filter(lambda x: str(x["id"]) == hand_item_id, list_hand_item))
+    filtered_enable = list(
+        filter(lambda x: str(x["id"]) == hand_item_id, list_hand_item)
+    )
     if filtered_enable:
         hand_item = Handitem(
             id=filtered_enable[0]["id"], name=filtered_enable[0]["name"]
@@ -32,3 +39,15 @@ def get_hand_item_by_id_or_api(hand_item_id: str):
         insert_handitem(handitem=hand_item)
         return hand_item
     return f"O enable com o id {hand_item_id} não foi encontrado."
+
+
+def get_enable_by_id_or_api(enable_id: str):
+    enable = get_enable_by_id(enable_id=enable_id)
+    if enable:
+        return enable
+
+    hb = HabbletApi()
+    list_enable = hb.get_badges()
+    filtered_enable = list(filter(lambda x: str(x["id"]) == enable_id, list_enable))
+    if filtered_enable:
+        enable = Badge(id=)
